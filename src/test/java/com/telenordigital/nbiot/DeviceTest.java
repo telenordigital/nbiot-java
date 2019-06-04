f1a75334db783aa07e4cd73f60d488d4015aa479
Gordon Klaus <gordon.klaus@telenordigital.com>
2019-06-04 15:18:38 +0200

    Randomly generate IMSI and IMEI to avoid collisions.

--- src/test/java/com/telenordigital/nbiot/DeviceTest.java
+++ src/test/java/com/telenordigital/nbiot/DeviceTest.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import org.junit.Test;
 
 public class DeviceTest {
@@ -14,11 +15,13 @@ public class DeviceTest {
 		tags.put("name", "The test collection");
 		Collection collection = client.createCollection(new ImmutableCollection.Builder().tags(tags).build());
 
+		Random rand = new Random();
+
 		try {
 			List<Device> devices = new ArrayList<>();
 			for (int i = 0; i < 10; i++) {
-				String imei = String.valueOf(2*i);
-				String imsi = String.valueOf(2*i+1);
+				String imei = String.valueOf(rand.nextInt((int)1e15));
+				String imsi = String.valueOf(rand.nextInt((int)1e15));
 				Device device = new ImmutableDevice.Builder().imei(imei).imsi(imsi).build();
 				device = client.createDevice(collection.id(), device);
 				devices.add(device);
